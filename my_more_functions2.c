4240610ef57e6e2a56bb91085f984c13c483992c
noura1se <you@example.com>
2023-10-20 13:33:45 -0700

    simple_shell

--- my_more_functions2.c
+++ my_more_functions2.c
@@ -53,20 +53,34 @@ fprintf(stderr, "%s: %d: %s: %s\n", info->fname
  */
 int print_d(int input, int fd)
 {
-FILE *output_stream = (fd == STDERR_FILENO) ? stderr : stdout;
-int count = 0;
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
 
-if (input < 0)
-{
-fputc('-', output_stream);
-count++;
-input = -input;
-}
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
 
-fprintf(output_stream, "%d", input);
-count += (int)(floor(log10(abs(input))) + 1);
-
-return (count);
+	return (count);
 }
 
 /**
