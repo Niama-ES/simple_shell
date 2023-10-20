02d24d56debbb88c4c51b549e8160ef7ea24bb3c
Niama-ES <developerzone@gmail.com>
2023-10-20 10:46:55 -0700

    simple_shell

--- my_chain.c
+++ my_chain.c
@@ -119,26 +119,26 @@ list_t *node;
 for (i = 0; info->argv[i]; i++)
 {
 if (info->argv[i][0] != '$' || !info->argv[i][1])
-continue;
+continue ;
 
 if (!_strcmp(info->argv[i], "$?"))
 {
 replace_string(&(info->argv[i]),
-_strdup(convert_number(info->status, 10, 0));
-continue;
+_strdup(convert_number(info->status, 10, 0)));
+continue ;
 }
 if (!_strcmp(info->argv[i], "$$"))
 {
 replace_string(&(info->argv[i]),
 _strdup(convert_number(getpid(), 10, 0)));
-continue;
+continue ;
 }
 node = node_starts_with(info->env, &info->argv[i][1], '=');
 if (node)
 {
 replace_string(&(info->argv[i]),
-_strdup(_strchr(node->str, '=') + 1);
-continue;
+_strdup(_strchr(node->str, '=') + 1));
+continue ;
 }
 replace_string(&(info->argv[i]), _strdup(""));
 }
